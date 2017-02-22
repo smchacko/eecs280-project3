455dca4ba86d066bacb5748dcb5466af81f4857a
Chacko, Shaun <smchacko@southquad16.ccs.itd.umich.edu>
2017-02-22 13:17:49 -0500

    test

---
+++ project3/project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project3
+//
+//  Created by Chacko, Shaun on 2/22/17.
+//  Copyright © 2017 Chacko, Shaun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
